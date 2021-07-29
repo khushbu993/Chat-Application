0d858dc4d8873178915dc1ac56f958f547ad7e55
Khushbu Islam Misbah <khushbuislamoffice@gmail.com>
2021-07-29 17:12:16 +0600

    user complete

--- routers/usersRouter.js
+++ routers/usersRouter.js
@@ -2,12 +2,33 @@
 const express = require("express");
 
 // internal imports
-const { getUsers } = require("../controllers/usersController");
+const {
+  getUsers,
+  addUser,
+  removeUser,
+} = require("../controllers/usersController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload = require("../middlewares/users/avatarUpload");
+const {
+  addUsersValidator,
+  addUserValidationHandler,
+} = require("../middlewares/users/usersValidator");
 
 const router = express.Router();
 
 // Users page
 router.get("/", decorateHtmlResponse("Users"), getUsers);
+
+// add User
+router.post(
+  "/",
+  avatarUpload,
+  addUsersValidator,
+  addUserValidationHandler,
+  addUser
+);
+
+// remove user
+router.delete("/:id", removeUser);
 
 module.exports = router;
